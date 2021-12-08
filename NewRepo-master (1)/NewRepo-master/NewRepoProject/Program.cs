1206e5212943f2448a6bf59de15d446639758857
ansbprdn <95761347+ansbprdn@users.noreply.github.com>
2021-12-08 23:05:34 +0700

    Add files via upload

---
+++ NewRepo-master (1)/NewRepo-master/NewRepoProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewRepoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pemograman lanjut S1 Informatika");
+            Console.WriteLine("Nama Kelompok");
+            Console.WriteLine("1. Annisa Budhi Pradani (20.11.3474)");
+            Console.WriteLine("2. Andrew Joshua Marthin L (20.11.3425)");
+            Console.WriteLine("3. Brimo Yogi Handiko (20.11.3475)");
+            Console.WriteLine("4. Muhammad Ammar (20.11.3471)");
+            Console.WriteLine("5. Paskalis Nani Randang (20.11.3470)");
+            Console.WriteLine("6. Sendi Tiantara(20.11.3473)");
+        }
+    }
+}
